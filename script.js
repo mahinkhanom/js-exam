56f4c1f2c62d1ab86c1b6ac37d437bd13faaa504
mahinkhanom <mahinkhanom58@gmail.com>
2024-09-14 11:15:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function nickname(){
+    document.getElementById("name").innerHTML ="My nickname is Shanti"
+}
+
+function myimage() {
+   document.getElementById('myimage').src='image/image/Mahin.jpg'
+  }
+
+function image(){
+    document.getElementById('myimage').src='image/sumu.jpg'
+}
+
+function mystyle(){
+   document.getElementById('style').style.color="green";
+}
+
+function hidetext(){
+    document.getElementById('hide').style.display='none'
+}
+function showtext(){
+    document.getElementById('show').style.display='block'
+}
